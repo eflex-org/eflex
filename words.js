7401bf7627850f6103f1fc1142ca2513a4d8ba0b
Akshay Rao <raoak@stanford.edu>
2026-04-20 13:45:34 -0700

    separate word flip library and increase pill size

---
+++ words.js
@@ -0,0 +1,14 @@
+export var words1 = ['Orchestrate', 'Design', 'Operate', 'Plan', 'Site'];
+
+export var words2 = [
+  'desalination', 'chlor-alkali', 'green hydrogen', 'water treatment',
+  'caustic soda', 'brine electrolysis', 'aluminum smelting',
+  'copper refining', 'lithium refining', 'semiconductor fabrication', 'battery recycling', 'waste-to-energy',
+  'biogas generation', 'carbon capture', 'paper pulping', 'textile dyeing',
+  'dairy processing', 'sugar refining', 'beer brewing', 'juice processing', 'starch processing', 'chemical manufacturing', 'soda ash',
+  'sodium hypochlorite', 'oxygen generation', 'nitrogen generation', 'ozone generation',
+  'electroplating', 'fuel cell', 'electrolysis', 'hydrogen peroxide',
+  'seawater electrolysis', 'CO₂ electrolysis', 'hydropower', 'nanofiltration', 'ultrafiltration', 'microfiltration',
+  'water reuse', 'potable reuse', 'agricultural reuse', 'industrial waste', 'wastewater reuse', 'greywater reuse', 'blackwater reuse',
+  'electrochemical reforming', 'electrochemical synthesis', 'electrochemical storage', 
+];
